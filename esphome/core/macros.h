72dcac3c92a28c8d24aeb66264f0cdcf8df3db19
Flaviu Tamas <me@flaviutamas.com>
2024-05-25 20:39:04 -0400

    Delete all unused files

--- esphome/core/macros.h
+++
@@ -1,4 +0,0 @@
-#pragma once
-
-// Helper macro to define a version code, whos evalue can be compared against other version codes.
-#define VERSION_CODE(major, minor, patch) ((major) << 16 | (minor) << 8 | (patch))
